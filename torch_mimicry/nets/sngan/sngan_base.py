18122ded443e7e0fe3f434a84db8752c5e97c75f
kwotsin <leeks_50996@hotmail.com>
2020-04-01 00:09:07 +0100

    Test autodoc

--- torch_mimicry/nets/sngan/sngan_base.py
+++ torch_mimicry/nets/sngan/sngan_base.py
@@ -9,10 +9,10 @@ class BaseGenerator(gan.BaseGenerator):
     ResNet backbone generator for SNGAN.
 
     Attributes:
-        - nz (int): Noise dimension for upsampling.
-        - ngf (int): Variable controlling generator feature map sizes.
-        - bottom_width (int): Starting width for upsampling generator output to an image.
-        - loss_type (str): Name of loss to use for GAN loss.
+        nz (int): Noise dimension for upsampling.
+        ngf (int): Variable controlling generator feature map sizes.
+        bottom_width (int): Starting width for upsampling generator output to an image.
+        loss_type (str): Name of loss to use for GAN loss.
     """
     def __init__(self, nz, ngf, bottom_width, loss_type='hinge', **kwargs):
         super().__init__(nz=nz,
@@ -27,7 +27,7 @@ class BaseDiscriminator(gan.BaseDiscriminator):
     ResNet backbone discriminator for SNGAN.
 
     Attributes:
-        - ndf (int): Variable controlling discriminator feature map sizes.
+        ndf (int): Variable controlling discriminator feature map sizes.
     """
     def __init__(self, ndf, loss_type='hinge', **kwargs):
         super().__init__(ndf=ndf, loss_type=loss_type, **kwargs)
