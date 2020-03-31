26b21c3dd7cadfc1b7d653eceab2bf90fc9c8363
kwotsin <leeks_50996@hotmail.com>
2020-03-31 23:55:45 +0100

    Test automodule

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 url = 'https://github.com/kwotsin/mimicry'
 
 install_requires = [
@@ -29,10 +29,10 @@ tests_require = ['pytest', 'pytest-cov', 'mock']
 long_description = """
 Mimicry is a lightweight PyTorch library aimed towards the reproducibility of GAN research.
 
-Comparing GANs is often difficult - mild differences in implementations and evaluation methodologies can result in huge performance differences. 
-Mimicry aims to resolve this by providing: 
-    (a) Standardized implementations of popular GANs that closely reproduce reported scores; 
-    (b) Baseline scores of GANs trained and evaluated under the same conditions; 
+Comparing GANs is often difficult - mild differences in implementations and evaluation methodologies can result in huge performance differences.
+Mimicry aims to resolve this by providing:
+    (a) Standardized implementations of popular GANs that closely reproduce reported scores;
+    (b) Baseline scores of GANs trained and evaluated under the same conditions;
     (c) A framework for researchers to focus on implementation of GANs without rewriting most of GAN training boilerplate code, with support for multiple GAN evaluation metrics.
 
 We provide a model zoo and set of baselines to benchmark different GANs of the same model size trained under the same conditions, using multiple metrics. To ensure reproducibility, we verify scores of our implemented models against reported scores in literature.
